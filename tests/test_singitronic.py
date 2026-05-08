23c18565b447b882e992543166d2eaa54a7b8b0c
Mubashir <mubashirhassan7259@gmail.com>
2026-05-08 22:15:04 +0500

    Update test_singitronic.py

--- tests/test_singitronic.py
+++ tests/test_singitronic.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
 BASE_URL = os.environ.get("APP_URL", "http://13.51.242.231").rstrip("/")
 
@@ -21,7 +20,8 @@ def driver():
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-extensions")
-    service = Service(ChromeDriverManager().install())
+    # Use the system chromedriver installed alongside google-chrome-stable
+    service = Service("/usr/bin/chromedriver")
     d = webdriver.Chrome(service=service, options=options)
     d.implicitly_wait(10)
     yield d
